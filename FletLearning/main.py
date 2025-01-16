731a8b840623fc2146b1641dc4e94540f1ec029a
Matheus Pereira Menezes <124296642+Mthh200@users.noreply.github.com>
2025-01-16 08:48:53 -0300

    Update main.py

--- FletLearning/main.py
+++ FletLearning/main.py
@@ -7,114 +7,115 @@ def main(page: ft.Page):
 
     page.appbar = ft.AppBar(
         leading = ft.Row([
-            ft.IconButton(icon= ft.Icons.CHEVRON_LEFT),
-            ft.TextButton(text = 'Equipes', style = ft.ButtonStyle(padding=0))
+            ft.IconButton(icon= ft.Icons.CHEVRON_LEFT, padding=0),
         ]),
         title = ft.Text('NaviWave'),
         center_title=True
     )
+
+    def go_to_equipe(view):
+        page.clean()
+        page.add(equipeView())
+        page.update()
+
+    def go_to_membro(route):
+        page.clean()
+        page.add(membrosView())
+
+    def membrosView():
+       
+        return ft.Container(
+            ft.Text('Membros View')
+        )
+        
     
-    def view_pop(view):
-        page.views.pop()
-        top_view = page.views[-1]
-        page.go(top_view.route)
 
-    def mudanca_de_rota(route):
-        page.go('/membro')
-        page.views.clear()
-        if page.route == '/membro':
-            page.views.append(
-                ft.View('/membro',
-                    [
-                        ft.Text('Tela de membro'),
-                        ft.Text('Em obras')
-                    ]
-                )
-            )
-
-
-    page.add(
-        ft.Card(
-            content=ft.Container(
-                content= ft.Column([
-                    ft.Row([
-                        ft.Text('Membros'),
-                        ft.IconButton(icon=ft.Icons.CHEVRON_RIGHT, on_click=mudanca_de_rota)
-                    ], spacing=160),
-                    ft.Row([
-                        ft.IconButton(icon=ft.Icons.PERSON_3),
-                        ft.IconButton(icon=ft.Icons.PERSON_3),
-                        ft.IconButton(icon=ft.Icons.PERSON_3),
-                        ft.IconButton(icon=ft.Icons.PERSON_3)
+    def equipeView():
+        return ft.Container(ft.Column([
+            ft.Card(
+                content=ft.Container(
+                    content= ft.Column([
+                        ft.Row([
+                            ft.Text('Membros', color='white'),
+                            ft.IconButton(icon=ft.Icons.CHEVRON_RIGHT, on_click=go_to_membro)
+                        ], spacing=210),
+                        ft.Column([
+                            ft.Row([
+                                ft.IconButton(icon=ft.Icons.PERSON_3),
+                                ft.IconButton(icon=ft.Icons.PERSON_3),
+                                ft.IconButton(icon=ft.Icons.PERSON_3),
+                                ft.IconButton(icon=ft.Icons.PERSON_3)
+                            ]),
+                            ft.Row([
+                                ft.IconButton(icon=ft.Icons.PERSON_3),
+                                ft.IconButton(icon=ft.Icons.PERSON_3),
+                                ft.IconButton(icon=ft.Icons.PERSON_3),
+                                ft.IconButton(icon=ft.Icons.PERSON_3)
+                            ])
+                        ])
                     ]),
-                    ft.Row([
-                        ft.IconButton(icon=ft.Icons.PERSON_3),
-                        ft.IconButton(icon=ft.Icons.PERSON_3),
-                        ft.IconButton(icon=ft.Icons.PERSON_3),
-                        ft.IconButton(icon=ft.Icons.PERSON_3)
-                    ])
-                ]),
-             width=320,
-             border_radius=10,
-             alignment=ft.alignment.center,
-             bgcolor='green',
-             padding=10
-            )
-        ),
-        ft.Container(content=ft.Column([
-            ft.Text('Produtividade da equipe'),
-            ft.Row([
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='#E1E1E1', border_radius=10, width=60, height=24)
-            ])
-        ])),
-        ft.Container(content=ft.Column([
-            ft.Text('Harmonia da equipe'),
-            ft.Row([
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='blue', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='#E1E1E1', border_radius=10, width=60, height=24),
-                ft.Container(bgcolor='#E1E1E1', border_radius=10, width=60, height=24)
-            ])
-        ])),
-        ft.Container(content=ft.Column([
-            ft.Row([
-                ft.Text('Pontos de Atenção'),
-                ft.IconButton(icon=ft.Icons.CHEVRON_RIGHT)
-            ], spacing=160),
-            ft.Row([
-                ft.Card(content= ft.Container(content=ft.Column([
-                    ft.IconButton(icon=ft.Icons.PERSON_3),
-                    ft.Text('Amanda', color='white')
-                ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER), 
-                width=146,
-                height=126,
-                border_radius=15,
+                width=330,
+                border_radius=10,
+                alignment=ft.alignment.center,
                 bgcolor='green',
-                alignment=ft.alignment.center
-                )),
-                ft.Card(content= ft.Container(content=ft.Column([
-                    ft.IconButton(icon=ft.Icons.PERSON_3),
-                    ft.Text('Gustavo', color='white')
-                ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER), 
-                width=146,
-                height=126,
-                border_radius=15,
-                bgcolor='green',
-                alignment=ft.alignment.center
-                ))
+                padding=5
+                )
+            ),
+            ft.Container(content=ft.Column([
+                ft.Text('Produtividade da equipe'),
+                ft.Row([
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='#E1E1E1', border_radius=10, width=55, height=24)
                 ])
-                
-            ]))
-    
+            ])),
+            ft.Container(content=ft.Column([
+                ft.Text('Harmonia da equipe'),
+                ft.Row([
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='blue', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='#E1E1E1', border_radius=10, width=55, height=24),
+                    ft.Container(bgcolor='#E1E1E1', border_radius=10, width=55, height=24)
+                ])
+            ])),
+            ft.Container(content=ft.Column([
+                ft.Row([
+                    ft.Text('Pontos de Atenção'),
+                    ft.IconButton(icon=ft.Icons.CHEVRON_RIGHT)
+                ], spacing=160),
+                ft.Row([
+                    ft.Card(content= ft.Container(content=ft.Column([
+                        ft.IconButton(icon=ft.Icons.PERSON_3),
+                        ft.Text('Amanda', color='white'),
+                        ft.Text('Publicitária', color='white')
+                    ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER), 
+                    width=146,
+                    height=126,
+                    border_radius=15,
+                    bgcolor='green',
+                    alignment=ft.alignment.center
+                    )),
+                    ft.Card(content= ft.Container(content=ft.Column([
+                        ft.IconButton(icon=ft.Icons.PERSON_3),
+                        ft.Text('Gustavo', color='white'),
+                        ft.Text('Desenvolvedor', color='white')
+                    ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER), 
+                    width=146,
+                    height=126,
+                    border_radius=15,
+                    bgcolor='green',
+                    alignment=ft.alignment.center
+                    ))
+                    ])
+                    
+                ]))
+            ])
+        
     )
-    page.on_route_change = mudanca_de_rota
-    page.on_view_pop = view_pop
-    page.go(page.route)
+    page.add(equipeView())
 
 
 ft.app(main)
